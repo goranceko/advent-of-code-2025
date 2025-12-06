f2ba257ac5981b0bc92b79f1f588dbb74679cdff
Goran Ceko <goran.ceko@dash0.com>
2025-12-06 15:15:15 +0100

    Day 6 part 2

--- day6/day6.go
+++ day6/day6.go
@@ -9,14 +9,14 @@ import (
 func main() {
 	fmt.Println("Advent of Code 2025 - Day 6")
 
-	fmt.Println("Part 1:", part1())
+	// fmt.Println("Part 1:", part1())
 	fmt.Println("\nPart 2:", part2())
 }
 
 func part1() int64 {
 	data, _ := os.ReadFile("input.txt")
 	lines := strings.Split(strings.TrimSpace(string(data)), "\n")
-	var sum int64
+
 	operations := strings.Fields(lines[4])
 
 	rows := make([][]string, 4)
@@ -24,18 +24,20 @@ func part1() int64 {
 		rows[i] = strings.Fields(lines[i])
 	}
 
+	var sum int64
 	for col := 0; col < len(operations); col++ {
-		num1 := num(rows[0][col])
-		num2 := num(rows[1][col])
-		num3 := num(rows[2][col])
-		num4 := num(rows[3][col])
+		result := num(rows[0][col])
 
-		switch operations[col] {
-		case "+":
-			sum += num1 + num2 + num3 + num4
-		case "*":
-			sum += num1 * num2 * num3 * num4
+		for row := 1; row < 4; row++ {
+			val := num(rows[row][col])
+			switch operations[col] {
+			case "+":
+				result += val
+			case "*":
+				result *= val
+			}
 		}
+		sum += result
 	}
 
 	return sum
@@ -48,5 +50,77 @@ func num(s string) int64 {
 }
 
 func part2() int64 {
-	return 0
+	data, _ := os.ReadFile("input.txt")
+	lines := strings.Split(string(data), "\n")
+
+	var sum int64
+
+	operations := lines[4]
+	currentOp := operations[0]
+	numbers := []int64{}
+	value := ""
+	for row := 0; row < 4; row++ {
+		value += string(lines[row][0])
+	}
+	numbers = append(numbers, num(value))
+
+	ops := []rune(operations)
+
+	for i := 1; i < len(lines[0]); i++ {
+		op := ops[i]
+		switch op {
+		case ' ':
+			value := ""
+			for row := 0; row < 4; row++ {
+				value += string(lines[row][i])
+			}
+			if strings.TrimSpace(value) == "" {
+				switch currentOp {
+				case '+':
+					result := int64(0)
+					for _, n := range numbers {
+						result += n
+					}
+					fmt.Println("result +:", result)
+					sum += result
+					fmt.Println("sum:", sum)
+				case '*':
+					result := int64(1)
+					for _, n := range numbers {
+						result *= n
+					}
+					fmt.Println("result *:", result)
+					sum += result
+					fmt.Println("sum:", sum)
+				}
+				numbers = nil
+			} else {
+				numbers = append(numbers, num(value))
+			}
+
+		case '+', '*':
+			currentOp = uint8(op)
+			value := ""
+			for row := 0; row < 4; row++ {
+				value += string(lines[row][i])
+			}
+			numbers = append(numbers, num(value))
+		}
+	}
+
+	if currentOp == '+' {
+		result := int64(0)
+		for _, n := range numbers {
+			result += n
+		}
+		sum += result
+	} else {
+		result := int64(1)
+		for _, n := range numbers {
+			result *= n
+		}
+		sum += result
+	}
+
+	return sum
 }
